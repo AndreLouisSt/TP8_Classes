7047c5b4c4c282825784e56bfe673e1faa59e5fa
AndreLouisSt <stephantandrelouis@gmail.com>
2025-03-13 09:21:45 +0000

    Debut

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+int main() {
+    vector<int> vecteurE (3);
+    vecteurE.push_back(1);
+    vecteurE.push_back(2);
+    vecteurE.push_back(3);
+}
