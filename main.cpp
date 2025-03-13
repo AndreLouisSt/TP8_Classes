23aa76907ffd3b3836d18a86e71a3e0ae373dda6
AndreLouisSt <stephantandrelouis@gmail.com>
2025-03-13 09:31:27 +0000

    gitignore

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <iostream>
 
 using namespace std;
 
@@ -9,4 +10,6 @@ int main() {
     vecteurE.push_back(1);
     vecteurE.push_back(2);
     vecteurE.push_back(3);
+    for(int i: vecteurE) 
+        cout << i << endl;
 }
